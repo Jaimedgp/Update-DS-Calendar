b211edf968fc98e646413148f588d9c9f4938155
Jaimedgp <jaime.diez.gp@gmail.com>
2019-10-25 20:14:04 +0200

    New organization

--- src/cal_setup.py
+++ src/cal_setup.py
@@ -7,15 +7,15 @@ from google.auth.transport.requests import Request
 # If modifying these scopes, delete the file token.pickle.
 SCOPES = ['https://www.googleapis.com/auth/calendar']
 
-CREDENTIALS_FILE = 'credentials.json'
+CREDENTIALS_FILE = 'doc/credentials.json'
 
 def get_calendar_service():
    creds = None
    # The file token.pickle stores the user's access and refresh tokens, and is
    # created automatically when the authorization flow completes for the first
    # time.
-   if os.path.exists('token.pickle'):
-       with open('token.pickle', 'rb') as token:
+   if os.path.exists('doc/token.pickle'):
+       with open('doc/token.pickle', 'rb') as token:
            creds = pickle.load(token)
    # If there are no (valid) credentials available, let the user log in.
    if not creds or not creds.valid:
@@ -27,7 +27,7 @@ def get_calendar_service():
            creds = flow.run_local_server(port=0)
 
        # Save the credentials for the next run
-       with open('token.pickle', 'wb') as token:
+       with open('doc/token.pickle', 'wb') as token:
            pickle.dump(creds, token)
 
    service = build('calendar', 'v3', credentials=creds)
