74c0e24beb66820068dc38d4f2ef6f1e308ba349
Jaimedgp <jaime.diez.gp@gmail.com>
2019-10-28 15:07:23 +0100

    get token for calendar and drive

--- src/cal_setup.py
+++ src/cal_setup.py
@@ -4,6 +4,7 @@ from googleapiclient.discovery import build
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
 
+
 # If modifying these scopes, delete the file token.pickle.
 SCOPES = ['https://www.googleapis.com/auth/calendar',
           'https://www.googleapis.com/auth/drive']
@@ -11,28 +12,28 @@ SCOPES = ['https://www.googleapis.com/auth/calendar',
 CREDENTIALS_FILE = '../doc/credentials.json'
 
 def get_services():
-   creds = None
-   service = [None, None]
-   # The file token.pickle stores the user's access and refresh tokens, and is
-   # created automatically when the authorization flow completes for the first
-   # time.
-   if os.path.exists('../doc/token.pickle'):
-       with open('../doc/token.pickle', 'rb') as token:
-           creds = pickle.load(token)
-   # If there are no (valid) credentials available, let the user log in.
-   if not creds or not creds.valid:
-       if creds and creds.expired and creds.refresh_token:
-           creds.refresh(Request())
-       else:
-           flow = InstalledAppFlow.from_client_secrets_file(
-               CREDENTIALS_FILE, SCOPES)
-           creds = flow.run_local_server(port=0)
+    creds = None
+    service = [None, None]
+    # The file token.pickle stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists('../doc/token.pickle'):
+        with open('../doc/token.pickle', 'rb') as token:
+            creds = pickle.load(token)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                CREDENTIALS_FILE, SCOPES)
+            creds = flow.run_local_server(port=0)
 
-       # Save the credentials for the next run
-       with open('../doc/token.pickle', 'wb') as token:
-           pickle.dump(creds, token)
+        # Save the credentials for the next run
+        with open('../doc/token.pickle', 'wb') as token:
+            pickle.dump(creds, token)
 
-   service[0] = build('calendar', 'v3', credentials=creds)
-   service[1] = build('drive', 'v3', credentials=creds)
+    service[0] = build('calendar', 'v3', credentials=creds)
+    service[1] = build('drive', 'v3', credentials=creds)
 
-   return service
+    return service
